e880365320df84a78c71cebc9d121298eb9e35bc
BEST-Thanawat <thanawat.sukwibul@gmail.com>
2021-03-24 16:47:32 -0700

    Doing some cosmetic.

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,9 +1,11 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flash_chat/constants.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 final _firestore = FirebaseFirestore.instance;
+User loggedInUser;
 
 class ChatScreen extends StatefulWidget {
   static const String id = 'chat_screen';
@@ -14,7 +16,7 @@ class ChatScreen extends StatefulWidget {
 class _ChatScreenState extends State<ChatScreen> {
   final messageTextController = TextEditingController();
   final _auth = FirebaseAuth.instance;
-  User loggedInUser;
+
   String messageText;
   bool showSpinner = false;
 
@@ -38,20 +40,6 @@ class _ChatScreenState extends State<ChatScreen> {
     }
   }
 
-  // void getMessages() async {
-  //   final messages = await _firestore.collection('messages').get();
-  //   for (var message in messages.docs) {
-  //     print(message.data());
-  //   }
-  // }
-  void messagesStream() async {
-    await for (var snapshot in _firestore.collection('messages').snapshots()) {
-      for (var message in snapshot.docs) {
-        print(message.data());
-      }
-    }
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -61,9 +49,8 @@ class _ChatScreenState extends State<ChatScreen> {
           IconButton(
               icon: Icon(Icons.close),
               onPressed: () {
-                messagesStream();
-                // _auth.signOut();
-                // Navigator.pop(context);
+                _auth.signOut();
+                Navigator.pop(context);
               }),
         ],
         title: Text('⚡️Chat'),
@@ -121,17 +108,24 @@ class MessagesStream extends StatelessWidget {
           return Center(child: CircularProgressIndicator(backgroundColor: Colors.lightBlueAccent));
         }
 
-        final messages = snapshot.data.docs;
+        final messages = snapshot.data.docs.reversed;
         for (var message in messages) {
           final messageText = message.data()['text'];
           final messageSender = message.data()['sender'];
 
-          final messageBubble = MessageBubble(sender: messageSender, text: messageText);
+          final currentUser = loggedInUser.email;
+
+          final messageBubble = MessageBubble(
+            sender: messageSender,
+            text: messageText,
+            isMe: currentUser == messageSender ? true : false,
+          );
           messageBubbles.add(messageBubble);
         }
 
         return Expanded(
           child: ListView(
+            reverse: true,
             padding: EdgeInsets.symmetric(horizontal: 10, vertical: 20),
             children: messageBubbles,
           ),
@@ -142,28 +136,34 @@ class MessagesStream extends StatelessWidget {
 }
 
 class MessageBubble extends StatelessWidget {
-  MessageBubble({this.sender, this.text});
+  MessageBubble({this.sender, this.text, this.isMe});
   final String sender;
   final String text;
+  final bool isMe;
 
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: EdgeInsets.all(10.0),
       child: Column(
-        crossAxisAlignment: CrossAxisAlignment.end,
+        crossAxisAlignment: isMe ? CrossAxisAlignment.end : CrossAxisAlignment.start,
         children: [
           Text(sender),
           Material(
-            borderRadius: BorderRadius.circular(30),
+            borderRadius: BorderRadius.only(
+              topLeft: isMe ? Radius.circular(30) : Radius.circular(0),
+              topRight: isMe ? Radius.circular(0) : Radius.circular(30),
+              bottomLeft: Radius.circular(30),
+              bottomRight: Radius.circular(30),
+            ),
             elevation: 5,
-            color: Colors.lightBlueAccent,
+            color: isMe ? Colors.lightBlueAccent : Colors.white,
             child: Padding(
               padding: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
               child: Text(
                 '$text',
                 style: TextStyle(
-                  color: Colors.white,
+                  color: isMe ? Colors.white : Colors.black54,
                   fontSize: 15,
                 ),
               ),
--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -31,11 +31,13 @@ class _LoginScreenState extends State<LoginScreen> {
             mainAxisAlignment: MainAxisAlignment.center,
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: <Widget>[
-              Hero(
-                tag: 'logo',
-                child: Container(
-                  height: 200.0,
-                  child: Image.asset('images/logo.png'),
+              Flexible(
+                child: Hero(
+                  tag: 'logo',
+                  child: Container(
+                    height: 200.0,
+                    child: Image.asset('images/logo.png'),
+                  ),
                 ),
               ),
               SizedBox(
@@ -48,21 +50,6 @@ class _LoginScreenState extends State<LoginScreen> {
                   email = value;
                 },
                 decoration: kTextFieldDecoration.copyWith(hintText: 'Enter your email'),
-                // decoration: InputDecoration(
-                //   hintText: 'Enter your email',
-                //   contentPadding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
-                //   border: OutlineInputBorder(
-                //     borderRadius: BorderRadius.all(Radius.circular(32.0)),
-                //   ),
-                //   enabledBorder: OutlineInputBorder(
-                //     borderSide: BorderSide(color: Colors.lightBlueAccent, width: 1.0),
-                //     borderRadius: BorderRadius.all(Radius.circular(32.0)),
-                //   ),
-                //   focusedBorder: OutlineInputBorder(
-                //     borderSide: BorderSide(color: Colors.lightBlueAccent, width: 2.0),
-                //     borderRadius: BorderRadius.all(Radius.circular(32.0)),
-                //   ),
-                // ),
               ),
               SizedBox(
                 height: 8.0,
@@ -74,21 +61,6 @@ class _LoginScreenState extends State<LoginScreen> {
                   password = value;
                 },
                 decoration: kTextFieldDecoration.copyWith(hintText: 'Enter your password'),
-                // decoration: InputDecoration(
-                //   hintText: 'Enter your password.',
-                //   contentPadding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
-                //   border: OutlineInputBorder(
-                //     borderRadius: BorderRadius.all(Radius.circular(32.0)),
-                //   ),
-                //   enabledBorder: OutlineInputBorder(
-                //     borderSide: BorderSide(color: Colors.lightBlueAccent, width: 1.0),
-                //     borderRadius: BorderRadius.all(Radius.circular(32.0)),
-                //   ),
-                //   focusedBorder: OutlineInputBorder(
-                //     borderSide: BorderSide(color: Colors.lightBlueAccent, width: 2.0),
-                //     borderRadius: BorderRadius.all(Radius.circular(32.0)),
-                //   ),
-                // ),
               ),
               SizedBox(
                 height: 24.0,
