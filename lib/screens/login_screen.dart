6911581282efd0d06b08bc0b016122b45a9765c2
BEST-Thanawat <thanawat.sukwibul@gmail.com>
2021-03-23 20:55:23 -0700

    Add Firebase.

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flash_chat/components/rounded_button.dart';
+import 'package:flash_chat/constants.dart';
+import 'package:flutter/services.dart';
 
 class LoginScreen extends StatefulWidget {
   static const String id = 'login_screen';
@@ -30,47 +32,53 @@ class _LoginScreenState extends State<LoginScreen> {
               height: 48.0,
             ),
             TextField(
+              textAlign: TextAlign.center,
+              keyboardType: TextInputType.emailAddress,
               onChanged: (value) {
                 //Do something with the user input.
               },
-              decoration: InputDecoration(
-                hintText: 'Enter your email',
-                contentPadding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.all(Radius.circular(32.0)),
-                ),
-                enabledBorder: OutlineInputBorder(
-                  borderSide: BorderSide(color: Colors.lightBlueAccent, width: 1.0),
-                  borderRadius: BorderRadius.all(Radius.circular(32.0)),
-                ),
-                focusedBorder: OutlineInputBorder(
-                  borderSide: BorderSide(color: Colors.lightBlueAccent, width: 2.0),
-                  borderRadius: BorderRadius.all(Radius.circular(32.0)),
-                ),
-              ),
+              decoration: kTextFieldDecoration.copyWith(hintText: 'Enter your email'),
+              // decoration: InputDecoration(
+              //   hintText: 'Enter your email',
+              //   contentPadding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+              //   border: OutlineInputBorder(
+              //     borderRadius: BorderRadius.all(Radius.circular(32.0)),
+              //   ),
+              //   enabledBorder: OutlineInputBorder(
+              //     borderSide: BorderSide(color: Colors.lightBlueAccent, width: 1.0),
+              //     borderRadius: BorderRadius.all(Radius.circular(32.0)),
+              //   ),
+              //   focusedBorder: OutlineInputBorder(
+              //     borderSide: BorderSide(color: Colors.lightBlueAccent, width: 2.0),
+              //     borderRadius: BorderRadius.all(Radius.circular(32.0)),
+              //   ),
+              // ),
             ),
             SizedBox(
               height: 8.0,
             ),
             TextField(
+              textAlign: TextAlign.center,
+              obscureText: true,
               onChanged: (value) {
                 //Do something with the user input.
               },
-              decoration: InputDecoration(
-                hintText: 'Enter your password.',
-                contentPadding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.all(Radius.circular(32.0)),
-                ),
-                enabledBorder: OutlineInputBorder(
-                  borderSide: BorderSide(color: Colors.lightBlueAccent, width: 1.0),
-                  borderRadius: BorderRadius.all(Radius.circular(32.0)),
-                ),
-                focusedBorder: OutlineInputBorder(
-                  borderSide: BorderSide(color: Colors.lightBlueAccent, width: 2.0),
-                  borderRadius: BorderRadius.all(Radius.circular(32.0)),
-                ),
-              ),
+              decoration: kTextFieldDecoration.copyWith(hintText: 'Enter your password'),
+              // decoration: InputDecoration(
+              //   hintText: 'Enter your password.',
+              //   contentPadding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+              //   border: OutlineInputBorder(
+              //     borderRadius: BorderRadius.all(Radius.circular(32.0)),
+              //   ),
+              //   enabledBorder: OutlineInputBorder(
+              //     borderSide: BorderSide(color: Colors.lightBlueAccent, width: 1.0),
+              //     borderRadius: BorderRadius.all(Radius.circular(32.0)),
+              //   ),
+              //   focusedBorder: OutlineInputBorder(
+              //     borderSide: BorderSide(color: Colors.lightBlueAccent, width: 2.0),
+              //     borderRadius: BorderRadius.all(Radius.circular(32.0)),
+              //   ),
+              // ),
             ),
             SizedBox(
               height: 24.0,
--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -3,6 +3,7 @@ import 'package:flash_chat/screens/registration_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:flash_chat/components/rounded_button.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 class WelcomeScreen extends StatefulWidget {
   static const String id = 'welcome_screen';
@@ -18,6 +19,8 @@ class _WelcomeScreenState extends State<WelcomeScreen> with SingleTickerProvider
   void initState() {
     // TODO: implement initState
     super.initState();
+
+    Firebase.initializeApp();
 
     controller = AnimationController(duration: Duration(seconds: 1), vsync: this);
     //animation = CurvedAnimation(parent: controller, curve: Curves.decelerate);
